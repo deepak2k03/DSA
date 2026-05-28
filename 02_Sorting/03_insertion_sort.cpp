a9a45e318491e3cc559090ed7f8861be2aac3f8b
Deepak <sman59472@gmail.com>
2026-05-28 22:10:28 +0530

    add implementation of insertion sort algorithm

---
+++ 02_Sorting/03_insertion_sort.cpp
@@ -0,0 +1,15 @@
+//takes an element and puts it in its correct position
+class Solution {
+  public:
+    void insertionSort(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]) {
+                swap(arr[j],arr[j-1]);
+                j--;
+            }
+        }
+    }
+};
