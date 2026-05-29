0c3735901cdf6e016bd9b7e9a51fe34fd4d2f43a
Deepak <sman59472@gmail.com>
2026-05-30 01:02:45 +0530

    add implementation for finding the single number in an array using bit manipulation

---
+++ 09_Bit_Manipulation/17_Single_number.cpp
@@ -0,0 +1,10 @@
+// User function template for C++
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+        // code here
+        int ans=0;
+        for(auto i:arr) ans^=i;
+        return ans;
+    }
+};
