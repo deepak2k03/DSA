51b650f6038caca8785036394f366b1ec884bc20
Deepak <sman59472@gmail.com>
2026-07-23 14:30:48 +0530

    add implementation for alpha triangle pattern

---
+++ 00_Basics/20_Alpha_triangle_pattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        char x='A'+n-1;
+        for(int j=0;j<i+1;j++){
+            cout<<x<<" ";
+            x--;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
